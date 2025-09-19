62a2499cd50c091f6632403caa98be73f1c13e2f
Duperlo72 <68718133+Duperlo72@users.noreply.github.com>
2025-09-19 00:22:02 -0500

    "Update E3"

--- Ejercicio_3.cpp
+++ Ejercicio_3.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
-int main(){
-  return 0
+using namespace std;
+void MCD(int a, int b){
+  int MaxComDiv = 1;
+  
+  for (int i = 1; i <= (a < b ? a : b); i++){
+    if(a%i==0 && b%i==0){
+      MaxComDiv = i;
     }
+  }
+  cout << "El MCD de " << a << " y " << b << " es: " << MaxComDiv << endl;  
+}
+int main(){
+  int num1, num2;
+  cout << "Ingrese dos numeros: ";
+  cin >> num1 >> num2;
+  MCD(num1, num2);
+  return 0;
+  }
