a83686e1514292947dd421987a0a3b6e1789c40f
joyita <andres07joya00@gmail.com>
2025-09-29 11:53:49 -0500

    menu_operaciones

--- callbacks.cpp
+++ callbacks.cpp
@@ -24,24 +24,58 @@ void performOperation(int (*callback)(int, int), int x, int y)
     cout << "The result of the operation is: " << callback(x, y) << endl;
 }
 
-int main()
-{
+int operadores(){
+    char operador;
+    cout << "Operadores" << endl;
+    cout << "(+). Suma" << endl;
+    cout << "(-). Multiplicacion" << endl;
+    cout << "(*). Resta" << endl;
+    cout << "(/) Dvision" << endl;
+    cout << "selecciones el operdor: " << endl;
+    cin >> operador;
+    return operador;
+}
+int main(){
+    int A, B;
+    char a;
+    char opc='s';
+    cout << "Ingrese el valor de A: ";
+    cin >> A;
+    cout << "Ingrese el valor de B: ";
+    cin >> B;
+
+    while (opc == 's'){
+        a= operadores();
+        switch (a){
+            case '+':
+                cout << "La suma es: " ;
+                performOperation(add, A, B);
+                break;
+            case '-':
+                cout << "La resta es: " ;
+                performOperation(subtract, A, B);
+                break;
+            case '*':
+                cout << "La multiplicacion es: ";
+                performOperation(multiply, A, B);
+                break;
+            default:
+                cout << "Operador no valido." << endl;
+                break;
+        }
+        cout << "Desea realizar otra operacion? (s/n): ";
+        cin >> opc;
+
+    }
+
     // Passing different functions as callbacks
-    cout << "Addition: ";
-    performOperation(add, 10, 5);
-
-    cout << "Multiplication: ";
-    performOperation(multiply, 10, 5);
-
-    cout << "Subtraction: ";
-    performOperation(subtract, 10, 5);
 
     int (*operations[])(int, int) = {add, multiply, subtract};
     // Iterate over the array and call each operation
     for (int i = 0; i < 3; ++i)
     {
         cout << "Operation " << i + 1 << ": ";
-        performOperation(operations[i], 5, 10);
+        performOperation(operations[i], A, B);
     }
     return 0;
 }
