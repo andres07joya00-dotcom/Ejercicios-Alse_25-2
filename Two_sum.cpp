080f6c8f7a84c11209d23b639d21c115a72e068d
joyita <andres07joya00@gmail.com>
2025-10-02 07:22:04 -0500

    defin class

--- Two_sum.cpp
+++ Two_sum.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
  
 using namespace std;
- 
-void sumatarget(int ar[4], int target, int arr[2]){
-    int a = 4;
-    for(int i = 0; i<a; i++){
-        for(int x = 0; x<a; x++){
-            if(ar[x] + ar[i] == target){
-                arr[0] = x;
-                arr[1] = i;
-            }
-        }   
-    };
-}
- 
+
+class Solution {
+public:
+    void sumatarget(int ar[4], int target, int arr[2]){
+        int a = 4;
+        for(int i = 0; i<a; i++){
+            for(int x = 0; x<a; x++){
+                if(ar[x] + ar[i] == target){
+                    arr[0] = x;
+                    arr[1] = i;
+                }
+            }   
+        };
+    }
+};
+
 int main(){
     int nums[4] = {0};
     int target = 0;
@@ -27,9 +30,11 @@ int main(){
     cin >> target;
 
     int arrrr[2];
-    sumatarget(nums, target, arrrr);
-    cout << "posicion en al arreglo: " << arrrr[0] << endl;
-    cout << "posicion en el arreglo: " << arrrr[1] << endl;
+    Solution Solution;
+    Solution.sumatarget(nums, target, arrrr);
+    cout << "indices de los numeros que suman el target: " << endl;
+    cout << arrrr[0] << endl;
+    cout << arrrr[1] << endl;
     
     return 0;
 }
